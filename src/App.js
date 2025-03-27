12de1184d97e72860abb36986470a5c1b8fd7945
Sunita <sunitamousaviii@gmail.com>
2025-03-27 18:12:53 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,30 +3,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer className="App-footer">
-        <footer className="text-center">
-          Coded by{" "}
-          <a
-            href="https://github.com/SunitaMousavi"
-            target="_blank"
-            rel="noopener noreferrer">
-            Sunita Mousavi
-          </a>
-          , open-sourced on{" "}
-          <a
-            href="https://github.com/SunitaMousavi/react-dictionary"
-            target="_blank"
-            rel="noopener noreferrer">
-            GitHub
-          </a>{" "}
-          and hosted on{" "}
-          <a
-            href="https://reactdictionaryapp-sunitamousavi.netlify.app"
-            target="_blank"
-            rel="noopener noreferrer">
-            Netlify
-          </a>
-        </footer>
+      <footer className="text-center">
+        Coded by{" "}
+        <a
+          href="https://github.com/SunitaMousavi"
+          target="_blank"
+          rel="noopener noreferrer">
+          Sunita Mousavi
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/SunitaMousavi/react-dictionary"
+          target="_blank"
+          rel="noopener noreferrer">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://reactdictionaryapp-sunitamousavi.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer">
+          Netlify
+        </a>
       </footer>
     </div>
   );
