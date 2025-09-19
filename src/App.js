e204891ee5e6c0b072b7c6c88ad5abd5cc92977f
Sunita <sunitamousaviii@gmail.com>
2025-09-19 21:07:36 +0330

    Added Github link and book logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
+import Books from "./Books.png";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+      <main>
+        <Dictionary />
+      </main>
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Books} alt="Books logo" />
       </header>
+      <footer className="App-footer text-center">
+        Coded by{" "}
+        <a
+          href="https://github.com/SunitaMousavi"
+          target="_blank"
+          rel="noopener noreferrer">
+          Sunita Mousavi
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/SunitaMousavi/react-dictionary-app"
+          target="_blank"
+          rel="noopener noreferrer">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://reactdictionaryapp-sunitamousavi.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer">
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  return "Hello from Dictionary";
+}
